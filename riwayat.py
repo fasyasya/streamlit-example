d3e76b856361602eae172d5f5a9110098bd12235
fasya nurkhayati <152140400+fasyasya@users.noreply.github.com>
2023-11-28 00:31:06 +0700

    Add files via upload

---
+++ riwayat.py
@@ -0,0 +1,44 @@
+
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Riwayat Sekolah | fasya",
+    page_icon="👨‍🎓",
+    layout="wide"
+)
+st.title("BEBERAPA JENJANG PENDIDIKAN YANG SAYA LALUI SELAMA MASA HIDUP")
+
+st.container()
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.subheader("MI misnaul ulum 2")
+
+with col2
+    st.subheader("mts plus Albukhori")
+
+with col3
+    st.subheader("MA Plus Albukhori")
+
+with col4:
+    st.subheader("UNU Rombel AL Bukhori")
+
+    st.image("MI.Jpg", width= 190)
+with col4:
+    st.subheader("UNIVERSITAS NAHDLATUL ULAMA")
+
+st.container()
+st.write("---")
+col1, col2, col3, col4 = st.columns(4)
+with col1
+
+    st.image("MI.JPg", width= 190)
+with col2
+    
+    st.image("mts.png", width= 190)
+with col3
+
+    st,image("ma.png", width= 190)
+with col4
+
+    st,image("unu.png", width= 190)
