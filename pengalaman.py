b67454e34f8eb434edcc20a21c0086babd1dff41
fasya nurkhayati <152140400+fasyasya@users.noreply.github.com>
2023-11-28 00:30:17 +0700

    Add files via upload

---
+++ pengalaman.py
@@ -0,0 +1,31 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Organizational Experience|fasya",
+    page_icon="👨‍🎓",
+    layout="wide"
+)
+
+st.title("Pengalaman Organisasi")
+st.write("Hanya Satu Organisasi Yang Saya Ikuti; ")
+
+st.container()
+col1, = st.columns(1)
+with col1:
+    st.subheader("panitia tahtiman Alfiyah")
+   
+
+st.container()
+st.write("---")
+col1, = st.columns(1)
+with col1:
+   
+    st.image("khataman.JPG", width= 360)
+
+st.container()
+st.write("---")
+col1, = st.columns(1)
+with col1:
+    st.write("*menjadi seksi dekdok 2 kali ini*")
+   
